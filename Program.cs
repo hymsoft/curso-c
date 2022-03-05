9d9b553b524f4e8686260ca0b769fce56081db7e
HyM Soft <hymsoft@gmail.com>
2022-03-04 21:20:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var age = 21;
+
+if (age >= 21)
+{
+    Console.WriteLine("Es mayor de edad");
+}
+else
+{
+    Console.WriteLine("No es mayor de edad");
+}
